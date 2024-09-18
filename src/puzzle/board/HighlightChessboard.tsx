c43f5d12fce7d4518132d827a1935c2e6fbdb543
Robert Blackwell <reblackwell3@gmail.com>
2024-09-18 11:37:47 -0400

    update method names to reflect they are not hooks

--- src/puzzle/board/HighlightChessboard.tsx
+++ src/puzzle/board/HighlightChessboard.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Chessboard } from 'react-chessboard';
 
-const useCheckHighlighting = (kingSquare: string, isCheck: boolean) => {
-    const styles: Record<string, any> = {};
+const getCheckHighlighting = (kingSquare: string, isCheck: boolean) => {
+  const styles: Record<string, any> = {};
   if (isCheck) {
     styles.kingSquare = { backgroundColor: 'red' };
   }
   return styles;
 };
 
-const useFeedbackHighlighting = (hintSquare: string | null, incorrectMoveSquare: string | null) => {
+const getFeedbackHighlighting = (
+  hintSquare: string | null,
+  incorrectMoveSquare: string | null,
+) => {
   const styles: Record<string, any> = {};
   if (hintSquare) {
     styles.hintSquare = { backgroundColor: '#77b1d4' };
@@ -27,11 +30,18 @@ export interface HighlightChessboardProps {
   incorrectMoveSquare: string | null;
 }
 
-const HighlightChessboard = (
-  { kingSquare, isCheck, hintSquare, incorrectMoveSquare, ...props }: HighlightChessboardProps
-) => {
-  const checkStyles = useCheckHighlighting(kingSquare, isCheck);
-  const feedbackStyles = useFeedbackHighlighting(hintSquare, incorrectMoveSquare);
+const HighlightChessboard = ({
+  kingSquare,
+  isCheck,
+  hintSquare,
+  incorrectMoveSquare,
+  ...props
+}: HighlightChessboardProps) => {
+  const checkStyles = getCheckHighlighting(kingSquare, isCheck);
+  const feedbackStyles = getFeedbackHighlighting(
+    hintSquare,
+    incorrectMoveSquare,
+  );
   const customSquareStyles = { ...checkStyles, ...feedbackStyles };
 
   return <Chessboard customSquareStyles={customSquareStyles} {...props} />;
