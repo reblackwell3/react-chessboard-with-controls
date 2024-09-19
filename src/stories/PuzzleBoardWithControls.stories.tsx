2f11ba03015ab18559471b6ed9b571d8bebf8573
Robert Blackwell <reblackwell3@gmail.com>
2024-09-19 17:48:18 -0400

    multi puzzles works

--- src/stories/PuzzleBoardWithControls.stories.tsx
+++ src/stories/PuzzleBoardWithControls.stories.tsx
@@ -71,7 +71,7 @@ export const Default: Story = {
   } as PuzzleBoardWithControlsProps,
 };
 
-const fetchPuzzles = async () => {
+const fetchPuzzles = () => {
   const puzzles = [
     {
       fen: 'r6k/pp2r2p/4Rp1Q/3p4/8/1N1P2R1/PqP2bPP/7K b - - 0 24',
